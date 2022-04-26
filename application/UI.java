04eaf15c47d618b88fa1f2b56862b508bd1b98e4
vinicius Prado <viniciusprado.prado@gmail.com>
2022-04-25 23:02:34 -0300

    Implementando os movimentos das torres.

--- application/UI.java
+++ application/UI.java
@@ -4,6 +4,9 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
 
+import javax.swing.*;
+import java.net.StandardSocketOptions;
+import java.sql.SQLOutput;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -29,6 +32,12 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
     public static ChessPosition readChessPosition (Scanner sc){
         try {
